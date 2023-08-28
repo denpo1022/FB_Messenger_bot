b18878f5e3a2bab9ee76fafd026241456f9d7cf2
denpo1022 <mario159357@gmail.com>
2023-08-28 23:20:33 +0800

    🙈 Update gitignore

--- messengerbot/common/core.py
+++ messengerbot/common/core.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
